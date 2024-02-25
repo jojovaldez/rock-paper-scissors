39ca2a4b1447a85861114c6c4052e57648a3c6e5
Jojo Valdez <jojo.valdez0307@icloud.com>
2024-02-25 01:50:24 -0600

    Computer's Play

--- game.cpp
+++ game.cpp
@@ -19,7 +19,17 @@ int main () {
     generatedNumber = (rand() % 3) + 1;
 
     //Generates Computers Play
+    if (generatedNumber == 1) {
+        computersPlay = "Rock";
+    }
+    else if (generatedNumber == 2) {
+        computersPlay = "Paper";
+    }
+    else {
+        computersPlay = "Scissors";
+    }
 
+    cout << "Hello World" << endl;
 
     //Generate Computer versus Player
 
